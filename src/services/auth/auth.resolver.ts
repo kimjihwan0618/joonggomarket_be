409f55bf2308f4215e9374e6cadb42a1ed71bbb7
kimJiHwan0618 <78426129+kimJiHwan0618@users.noreply.github.com>
2024-11-25 11:45:12 +0900

    응답 헤더에 domain 속성 제거하여 토큰 세팅 구현

--- src/services/auth/auth.resolver.ts
+++ src/services/auth/auth.resolver.ts
@@ -30,7 +30,7 @@ export class AuthResolver {
       secure: process.env.NODE_ENV === 'production',
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', // 교차 도메인 쿠키 허용
       maxAge: token ? 7 * 24 * 60 * 60 * 1000 : 0,
-      domain: process.env.DOMAIN,
+      // domain: process.env.DOMAIN, // cors 쿠키 사용시 domain 입력 x
       // maxAge: token ? 60 * 60 * 1000 : 0,
     });
     return result;
@@ -50,7 +50,7 @@ export class AuthResolver {
       secure: process.env.NODE_ENV === 'production', // HTTPS에서만 사용
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
       maxAge: token ? 7 * 24 * 60 * 60 * 1000 : 0, // 1주일 동안 유효
-      domain: process.env.DOMAIN,
+      // domain: process.env.DOMAIN,
       // maxAge: token ? 60 * 60 * 1000 : 0, // 1분 동안 유효
     });
     return result;
